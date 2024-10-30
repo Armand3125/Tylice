d28c4fcf48af53f774f149d6b5ae8e53436a5637
Armand3125 <armandjulien05@gmail.com>
2024-10-30 13:28:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,7 @@ def traiter_img(img, Nc, Nd, dim_max):
             button_color = f"rgb({rgb[0]}, {rgb[1]}, {rgb[2]})"
             
             # Utiliser st.button avec une clé unique pour chaque bouton
-            if st.button(f"{col_name}", key=f"btn_{i}_{j}", help=f"Couleur: {col_name}", 
-                         style=f"background-color: {button_color}; color: white;"):
+            if st.button(f"{col_name}", key=f"btn_{i}_{j}"):
                 st.session_state.selected_colors[i] = j  # Mémoriser la couleur sélectionnée
                 selected_color_message = f"Vous avez sélectionné: {col_name}"  # Mettre à jour le message
 
