f81aed7a4e9fc4dfb462577f43fd6be27acda6cc
Armand3125 <armandjulien05@gmail.com>
2024-11-04 17:12:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,11 +55,11 @@ def traiter_img(img, Nc, Nd, dim_max):
         new_img_arr = nouvelle_img(img_arr, labels, cl_proches, st.session_state.selected_colors, pal)
         st.session_state.modified_image = new_img_arr.astype('uint8')
 
-        return sorted_cls, total_px, cl_proches, img_arr  # Ajoutez ces valeurs pour les rendre accessibles
+        return sorted_cls, total_px, cl_proches, img_arr, labels  # Ajoutez labels au retour
 
     except Exception as e:
         st.error(f"Une erreur est survenue : {e}")
-        return None, None, None, None  # Retournez None en cas d'erreur
+        return None, None, None, None, None  # Retournez None pour tous les retours en cas d'erreur
 
 # Interface Streamlit
 st.title("Traitement d'Image avec Palette de Couleurs")
@@ -70,16 +70,16 @@ Nd = st.slider("Nombre de Couleurs dans la Palette", 2, len(pal), 6)
 # Fixer la dimension maximale de l'image à 400
 dim_max = 400  
 
-sorted_cls, total_px, cl_proches, img_arr = None, None, None, None  # Initialiser ces variables
+sorted_cls, total_px, cl_proches, img_arr, labels = None, None, None, None, None  # Initialiser ces variables
 
 if uploaded_file is not None:
-    sorted_cls, total_px, cl_proches, img_arr = traiter_img(uploaded_file, Nc, Nd, dim_max)
+    sorted_cls, total_px, cl_proches, img_arr, labels = traiter_img(uploaded_file, Nc, Nd, dim_max)
 
 if 'modified_image' in st.session_state:
     st.image(st.session_state.modified_image, caption="Image Modifiée", width=int(1.5 * dim_max))
 
 # Vérifiez si sorted_cls et total_px ne sont pas None avant de les utiliser
-if sorted_cls is not None and total_px is not None:
+if sorted_cls is not None and total_px is not None and labels is not None:
     for idx, (cl, count) in enumerate(sorted_cls):
         percentage = (count / total_px) * 100
         st.write(f"Cluster {idx + 1} - {percentage:.2f}%")
@@ -105,4 +105,3 @@ if sorted_cls is not None and total_px is not None:
                 st.session_state.selected_colors[cl] = j
                 new_img_arr = nouvelle_img(img_arr, labels, cl_proches, st.session_state.selected_colors, pal)
                 st.session_state.modified_image = new_img_arr.astype('uint8')
-
