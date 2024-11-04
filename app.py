0ec88d7cdf2381c6a3b13a3c62a8ec4f7c7eefe5
Armand3125 <armandjulien05@gmail.com>
2024-11-04 13:17:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from scipy.spatial import distance
 from PIL import Image
 import numpy as np
 
+# Palette de couleurs définie
 pal = {
     "Noir_Charbon": (0, 0, 0), "Blanc_Jade": (255, 255, 255),
     "Jaune_Or": (228, 189, 104), "Bleu_Cyan": (0, 134, 214),
@@ -14,6 +15,7 @@ pal = {
     "Gris_Argent": (166, 169, 170), "Violet_Basic": (94, 67, 183),
 }
 
+# Calculer les couleurs les plus proches
 def proches(c, pal):
     dists = [(n, distance.euclidean(c, col)) for n, col in pal.items()]
     return sorted(dists, key=lambda x: x[1])
@@ -21,11 +23,13 @@ def proches(c, pal):
 def proches_lim(c, pal, n):
     return [n for n, _ in proches(c, pal)[:n]]
 
+# Créer une nouvelle image en mappant les clusters aux couleurs de la palette
 def nouvelle_img(img_arr, labels, cl_proches, selected_colors, pal):
     color_map = {i: pal[cl_proches[i][selected_colors[i]]] for i in range(len(cl_proches))}
     img_mapped = np.array([color_map[label] for label in labels])
     return img_mapped.reshape(img_arr.shape)
 
+# Traiter l'image pour clustering et application de la palette
 def traiter_img(img, Nc, Nd, dim_max):
     try:
         img = Image.open(img).convert('RGB')
@@ -41,7 +45,6 @@ def traiter_img(img, Nc, Nd, dim_max):
         cl_counts = dict(zip(uniq, counts))
 
         sorted_cls = sorted(cl_counts.items(), key=lambda x: x[1], reverse=True)
-
         cl_proches = [proches_lim(kmeans.cluster_centers_[i], pal, Nd) for i in cl_counts.keys()]
 
         if 'selected_colors' not in st.session_state:
@@ -60,22 +63,29 @@ def traiter_img(img, Nc, Nd, dim_max):
                 rgb = pal[color]
                 rgb_str = f"rgb({rgb[0]}, {rgb[1]}, {rgb[2]})"
                 
+                # Générer un style pour le bouton en fonction de la sélection actuelle
                 button_style = f"background-color: {rgb_str}; width: 50px; height: 20px;"
                 if st.session_state.selected_colors[cluster_idx] == j:
                     button_style += " border: 3px solid red;"
                 else:
                     button_style += " border: 1px solid black;"
 
-                if cols[j].button(label="", key=f'button_{cluster_idx}_{j}', help=color, use_container_width=True):
+                # Utiliser une clé unique pour chaque bouton en fonction du cluster et de l'option de couleur
+                button_key = f'button_{cluster_idx}_{j}'
+                
+                # Vérification et mise à jour de la sélection
+                if cols[j].button(label="", key=button_key, help=color, use_container_width=True):
                     st.session_state.selected_colors[cluster_idx] = j
                     new_img_arr = nouvelle_img(img_arr, labels, cl_proches, st.session_state.selected_colors, pal)
                     st.session_state.modified_image = new_img_arr.astype('uint8')
 
+                # Afficher le bouton avec le style HTML
                 cols[j].markdown(f"<div style='{button_style}'></div>", unsafe_allow_html=True)
 
     except Exception as e:
         st.error(f"Une erreur est survenue : {e}")
 
+# Interface Streamlit
 st.title("Traitement d'Image avec Palette de Couleurs")
 uploaded_file = st.file_uploader("Choisissez une image", type=["jpg", "jpeg", "png"])
 Nc = st.slider("Nombre de Clusters", 2, 7, 4)
