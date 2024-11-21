f642ae9368ee94afb5a6b7115e6d775855da22a4
Armand3125 <armandjulien05@gmail.com>
2024-11-21 14:46:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.metrics import pairwise_distances_argmin_min
+import io
+from datetime import datetime
 
-# Palette de couleurs
 pal = {
     "NC": (0, 0, 0), "BJ": (255, 255, 255),
     "JO": (228, 189, 104), "BC": (0, 134, 214),
@@ -13,7 +13,7 @@ pal = {
     "OM": (249, 153, 99), "VGa": (59, 102, 94),
     "BG": (163, 216, 225), "VM": (236, 0, 140),
     "GA": (166, 169, 170), "VB": (94, 67, 183),
-    "BF": (4, 47, 86), 
+    "BF": (4, 47, 86),
 }
 
 st.title("Tylice")
@@ -22,10 +22,12 @@ css = """
     <style>
         .stRadio div [data-testid="stMarkdownContainer"] p { display: none; }
         .radio-container { display: flex; flex-direction: column; align-items: center; margin: 0; }
-        .color-container { display: flex; flex-direction: column; align-items: center; }
+        .color-container { display: flex; flex-direction: column; align-items: center; margin-top: 5px; }
         .color-box { border: 3px solid black; }
         .stColumn { padding: 0 !important; }
         .first-box { margin-top: 15px; }
+        .percentage-container { margin-bottom: 0; }
+        .button-container { margin-bottom: 20px; }
     </style>
 """
 st.markdown(css, unsafe_allow_html=True)
@@ -35,40 +37,35 @@ uploaded_image = st.file_uploader("Télécharger une image", type=["jpg", "jpeg"
 if "num_selections" not in st.session_state:
     st.session_state.num_selections = 4
 
-col1, col2 = st.columns([1, 5])
+col1, col2 = st.columns([2, 5])
 
 with col1:
-    if st.button("4 Couleurs"):
+    if st.button("4 Couleurs : 7.95 €"):
         st.session_state.num_selections = 4
 
 with col2:
-    if st.button("6 Couleurs"):
+    if st.button("6 Couleurs : 11.95 €"):
         st.session_state.num_selections = 6
 
 num_selections = st.session_state.num_selections
+cols_percentages = st.columns(num_selections)
+
 rectangle_width = 80 if num_selections == 4 else 50
 rectangle_height = 20
 cols = st.columns(num_selections * 2)
 
 if uploaded_image is not None:
-    # Assure que l'image est en RGB pour éviter les problèmes liés au canal alpha ou au mode niveaux de gris
     image = Image.open(uploaded_image).convert("RGB")
     width, height = image.size
-    if width > height:
-        new_width = 400
-        new_height = int((new_width / width) * height)
-    else:
-        new_height = 400
-        new_width = int((new_height / height) * width)
-    
+    dim = 350
+    new_width = dim if width > height else int((dim / height) * width)
+    new_height = dim if height >= width else int((dim / width) * height)
+
     resized_image = image.resize((new_width, new_height))
     img_arr = np.array(resized_image)
-    
-    # Vérifie que l'image est bien en RGB
+
     if img_arr.shape[-1] == 3:
         pixels = img_arr.reshape(-1, 3)
-        
-        # Clustering et sélection de couleurs
         kmeans = KMeans(n_clusters=num_selections, random_state=0).fit(pixels)
         labels = kmeans.labels_
         centers = kmeans.cluster_centers_
@@ -82,11 +79,24 @@ if uploaded_image is not None:
             closest_colors_idx = distances[i].argsort()
             ordered_colors_by_cluster.append([list(pal.keys())[idx] for idx in closest_colors_idx])
 
+        cluster_counts = np.bincount(labels)
+        total_pixels = len(labels)
+        cluster_percentages = (cluster_counts / total_pixels) * 100
+
+        sorted_indices = np.argsort(-cluster_percentages)
+        sorted_percentages = cluster_percentages[sorted_indices]
+        sorted_ordered_colors_by_cluster = [ordered_colors_by_cluster[i] for i in sorted_indices]
+
+        for i, percentage in enumerate(sorted_percentages):
+            with cols_percentages[i]:
+                st.markdown(f"<div class='percentage-container'><b>{percentage:.2f}%</b></div>", unsafe_allow_html=True)
+
         selected_colors = []
-        for i in range(num_selections):
+        selected_color_names = []
+        for i, cluster_index in enumerate(sorted_indices):
             with cols[i * 2]:
                 st.markdown("<div class='color-container'>", unsafe_allow_html=True)
-                for j, color_name in enumerate(ordered_colors_by_cluster[i]):
+                for j, color_name in enumerate(sorted_ordered_colors_by_cluster[i]):
                     color_rgb = pal[color_name]
                     margin_class = "first-box" if j == 0 else ""
                     st.markdown(
@@ -96,21 +106,38 @@ if uploaded_image is not None:
                 st.markdown("</div>", unsafe_allow_html=True)
 
             with cols[i * 2 + 1]:
-                selected_color_name = st.radio("", ordered_colors_by_cluster[i], key=f"radio_{i}", label_visibility="hidden")
+                selected_color_name = st.radio("", sorted_ordered_colors_by_cluster[i], key=f"radio_{i}", label_visibility="hidden")
                 selected_colors.append(pal[selected_color_name])
+                selected_color_names.append(selected_color_name)
 
         new_img_arr = np.zeros_like(img_arr)
         for i in range(img_arr.shape[0]):
             for j in range(img_arr.shape[1]):
                 lbl = labels[i * img_arr.shape[1] + j]
-                new_img_arr[i, j] = selected_colors[lbl]
-        
+                new_color_index = np.where(sorted_indices == lbl)[0][0]
+                new_img_arr[i, j] = selected_colors[new_color_index]
+
         new_image = Image.fromarray(new_img_arr.astype('uint8'))
-        width, height = new_image.size
-        resized_image = new_image.resize((int(width * 1.1), int(height * 1.1)))
+        resized_image = new_image
 
         col1, col2, col3 = st.columns([1, 6, 1])
         with col2:
-            st.image(resized_image, caption=f"Image avec {num_selections} couleurs", use_column_width=True)
+            st.image(resized_image, use_column_width=True)
+
+        img_buffer = io.BytesIO()
+        new_image.save(img_buffer, format="PNG")
+        img_buffer.seek(0)
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_name = f"{''.join(selected_color_names)}_{timestamp}.png"
+
+        col1, col2, col3 = st.columns([6, 4, 6])
+        with col2:
+            st.download_button(
+                label="Télécharger l'image",
+                data=img_buffer,
+                file_name=file_name,
+                mime="image/png"
+            )
     else:
         st.error("L'image doit être en RGB (3 canaux) pour continuer.")
